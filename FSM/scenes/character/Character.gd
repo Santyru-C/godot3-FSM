37b90eb8c5f435a4b1c5bf16ded3651eb862d886
Santyru <santiago0988@hotmail.com>
2023-07-06 20:03:57 -0300

    add update_input function

--- FSM/scenes/character/Character.gd
+++ FSM/scenes/character/Character.gd
@@ -1,5 +1,18 @@
 extends KinematicBody2D
 
+var direction : Vector2 = Vector2(0, 1)
+var velocity : Vector2 = Vector2.ZERO
+var last_direction : int
+
 func _ready():
 	$AnimationTree.get("parameters/playback").start("idle")
 	
+func update_input():
+	direction.x = (
+		Input.get_action_strength("ui_right") 
+		- Input.get_action_strength("ui_left")
+	)
+
+func _physics_process(delta):
+	update_input()
+	print(direction.x)
