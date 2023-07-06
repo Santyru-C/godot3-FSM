ed58f173da9625b3f30b880d8bafd0c8bf18fd4b
Santyru <santiago0988@hotmail.com>
2023-07-06 18:52:40 -0300

    add formated string to no State Object child error

--- FSM/nodes/StateMachine.gd
+++ FSM/nodes/StateMachine.gd
@@ -5,21 +5,22 @@ export(NodePath) var character_path
 export(NodePath) var animation_tree_path
 
 var states = []
-onready var current_state : State = null
+onready var current_state : State = $Idle
 
 func _ready():
 	var character = get_node(character_path)
 	var animation_tree = get_node(animation_tree_path)
 	
 	for child in get_children():
+		print(child)
 		if child is State:
 			child.character = character
 			child.animation_tree = animation_tree
-			child.animation.node = animation_tree.get("parameters/playback")
+			child.animation_node = animation_tree.get("parameters/playback")
 			states.append(child)
 			
 		else:
-			push_warning(child + " node is not a State object!")
+			push_warning("%s is not a State Object!" % child)
 
 func switch_states(new_state : State):
 	if (current_state != null):
@@ -30,7 +31,6 @@ func switch_states(new_state : State):
 		current_state.on_enter()
 
 func _physics_process(delta): #update
-	print(current_state)
 	if (current_state.next_state != null):
 		switch_states(current_state.next_state)
 	
