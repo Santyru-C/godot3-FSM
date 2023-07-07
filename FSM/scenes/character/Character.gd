3188c96c463f9d0c7c408a6466c1f7d2bbb94e48
Santyru <santiago0988@hotmail.com>
2023-07-06 21:18:41 -0300

    add accelerate function

--- FSM/scenes/character/Character.gd
+++ FSM/scenes/character/Character.gd
@@ -13,6 +13,23 @@ func update_input():
 		- Input.get_action_strength("ui_left")
 	)
 
+func update_sprite_flip():
+	#var h_flip = $Sprite.get("flip_h")
+	$Sprite.flip_h = false if last_direction > 0 else true
+	print(direction.x)
+	
+func accelerate(delta, h_acceleration, max_speed):
+	if last_direction != direction.x:
+		velocity.x /= 4 # reduce speed for faster turning
+		
+	velocity.x += h_acceleration * direction.x * delta
+	
+	if abs(velocity.x) >= max_speed:
+		velocity.x = max_speed * direction.x
+
 func _physics_process(delta):
 	update_input()
-	print(direction.x)
+	if direction.x != 0:
+		last_direction = direction.x
+	update_sprite_flip()
+	velocity = move_and_slide(velocity, Vector2.UP)
