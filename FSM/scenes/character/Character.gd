e4041a5acb107784c1513f308e8ed4355b8a5c3e
Santyru <santiago0988@hotmail.com>
2023-07-08 17:27:03 -0300

    add movement behaviour

--- FSM/scenes/character/Character.gd
+++ FSM/scenes/character/Character.gd
@@ -4,8 +4,21 @@ var direction : Vector2 = Vector2(0, 1)
 var velocity : Vector2 = Vector2.ZERO
 var last_direction : int
 
+export(int) var max_speed = 500
+export(float) var t_max_speed = 0.5
+export(float) var t_stop = 0.5
+export(int) var max_jump_height = 175
+export(float) var t_max_height = 0.5
+
+onready var h_acceleration : float = (max_speed / pow(t_max_speed, 2))
+onready var h_friction : float = (max_speed/ t_stop)
+onready var jump_velocity = (2 * max_jump_height) / t_max_height * -1
+onready var jump_gravity = (2 * max_jump_height) / pow(t_max_height, 2)
+var current_gravity : float = 0
+
 func _ready():
 	$AnimationTree.get("parameters/playback").start("idle")
+	current_gravity = jump_gravity
 	
 func update_input():
 	direction.x = (
@@ -16,20 +29,35 @@ func update_input():
 func update_sprite_flip():
 	#var h_flip = $Sprite.get("flip_h")
 	$Sprite.flip_h = false if last_direction > 0 else true
-	print(direction.x)
 	
-func accelerate(delta, h_acceleration, max_speed):
-	if last_direction != direction.x:
-		velocity.x /= 4 # reduce speed for faster turning
+func accelerate(delta):
+
+	# reduce speed for faster turning
 		
 	velocity.x += h_acceleration * direction.x * delta
 	
 	if abs(velocity.x) >= max_speed:
 		velocity.x = max_speed * direction.x
 
+func desaccelerate(delta):
+	if velocity.x > 1:
+		velocity.x -= h_friction * delta
+	if velocity.x < -1:
+		velocity.x += h_friction * delta
+	if abs(velocity.x) < 0.1:
+		velocity.x = 0
+		
+func jump():
+	velocity.y = jump_velocity
+	
+func apply_gravity(delta):
+	velocity.y += current_gravity * delta
+	
 func _physics_process(delta):
 	update_input()
 	if direction.x != 0:
 		last_direction = direction.x
+	
 	update_sprite_flip()
+	apply_gravity(delta)
 	velocity = move_and_slide(velocity, Vector2.UP)
