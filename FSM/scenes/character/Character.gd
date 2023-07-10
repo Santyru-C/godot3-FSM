89513eae8056a679b34c0577656a13aafd4b8321
Santyru <santiago0988@hotmail.com>
2023-07-10 17:37:19 -0300

    add coyote time

--- FSM/scenes/character/Character.gd
+++ FSM/scenes/character/Character.gd
@@ -10,7 +10,7 @@ export(float) var t_stop = 0.5
 export(int) var max_jump_height = 175
 export(float) var t_max_height = 0.5
 
-onready var h_acceleration : float = (max_speed / pow(t_max_speed, 2))
+onready var h_acceleration : float = (max_speed / t_max_speed)
 onready var h_friction : float = (max_speed/ t_stop)
 onready var jump_velocity = (2 * max_jump_height) / t_max_height * -1
 onready var jump_gravity = (2 * max_jump_height) / pow(t_max_height, 2)
@@ -31,8 +31,9 @@ func update_sprite_flip():
 	$Sprite.flip_h = false if last_direction > 0 else true
 	
 func accelerate(delta):
-
 	# reduce speed for faster turning
+	if sign(velocity.x) != direction.x:
+		velocity.x /= 2
 		
 	velocity.x += h_acceleration * direction.x * delta
 	
@@ -40,11 +41,9 @@ func accelerate(delta):
 		velocity.x = max_speed * direction.x
 
 func desaccelerate(delta):
-	if velocity.x > 1:
-		velocity.x -= h_friction * delta
-	if velocity.x < -1:
-		velocity.x += h_friction * delta
-	if abs(velocity.x) < 0.1:
+	velocity.x -= (velocity.x / t_stop) * delta
+
+	if abs(velocity.x) < 5:
 		velocity.x = 0
 		
 func jump():
@@ -52,12 +51,12 @@ func jump():
 	
 func apply_gravity(delta):
 	velocity.y += current_gravity * delta
+	velocity.y = min(velocity.y, 1500) # avoid gaining masive speed when falling
 	
 func _physics_process(delta):
 	update_input()
 	if direction.x != 0:
 		last_direction = direction.x
-	
-	update_sprite_flip()
 	apply_gravity(delta)
-	velocity = move_and_slide(velocity, Vector2.UP)
+	update_sprite_flip()
+	velocity = move_and_slide(velocity, Vector2.UP) # reassign to remove excess velocity.
