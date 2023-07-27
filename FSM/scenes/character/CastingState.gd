f7689129b49a28f2a25d4de9bf2be303766465b7
Santyru <santiago0988@hotmail.com>
2023-07-27 17:26:45 -0300

    add new state

---
+++ FSM/scenes/character/CastingState.gd
@@ -0,0 +1,12 @@
+extends State
+
+export(NodePath) var idle_state_path
+
+onready var idle_state = get_node(idle_state_path)
+
+func on_enter():
+	animation_node.travel("casting")
+	character.emit_signal("spell_casted")
+
+func on_exit():
+	next_state = idle_state
