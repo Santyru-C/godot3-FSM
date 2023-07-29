c1eaeae8a6a474e9bc2a553619b224f9df97b7d5
Santyru <santiago0988@hotmail.com>
2023-07-28 22:10:39 -0300

    add functionality

--- FSM/scenes/environment/platforms/platform.gd
+++ FSM/scenes/environment/platforms/platform.gd
@@ -1,4 +1,21 @@
 extends StaticBody2D
 
-func change_state():
-	pass
+export(Texture) var sprite_active
+export(Texture) var sprite_disabled
+
+onready var collision_shape = $CollisionShape2D
+onready var sprite_texture = $Sprite.texture
+
+func enable_platform():
+	collision_shape.disabled = false
+	$Sprite.texture = sprite_active
+	
+func disable_platform():
+	collision_shape.disabled = true
+	$Sprite.texture = sprite_disabled
+
+func toggle_state():
+	if collision_shape.disabled:
+		enable_platform()
+	else:
+		disable_platform()
